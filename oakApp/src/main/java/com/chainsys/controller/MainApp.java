ef2e8c6815fff58c834ab6c8037773783330b619
HarithaLakshmi017 <105410242+HarithaLakshmi017@users.noreply.github.com>
2022-07-25 15:53:40 +0530

    first commit

---
+++ oakApp/src/main/java/com/chainsys/controller/MainApp.java
@@ -0,0 +1,10 @@
+package com.chainsys.controller;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+@SpringBootApplication
+public class MainApp {
+    public static void main(String args[]) {
+        SpringApplication.run(MainApp.class, args);
+    }
+
+}
